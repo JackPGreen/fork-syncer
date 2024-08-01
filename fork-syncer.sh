941cd30c39e47b80835f551b3e91c04679d5b512
Jack Green <JackPGreen@Gmail.com>
2024-08-01 15:05:06 +0100

    Support >30 repositories
    By default, [the output of `gh repo list` is limited to 30 repositories](https://cli.github.com/manual/gh_repo_list)

--- fork-syncer.sh
+++ fork-syncer.sh
@@ -14,7 +14,7 @@ check_command "gh" "https://cli.github.com/"
 # https://github.com/mislav/hub/issues/2923#issuecomment-1062045270
 github_username=$(gh api user --jq '.login')
 
-repos=$(gh repo list --fork --json name --jq '.[]' "${github_username}")
+repos=$(gh repo list --fork --limit 9999 --json name --jq '.[]' "${github_username}")
 
 while read -r repo_data; do
   repo_name=$(echo "${repo_data}" | jq -r '.name')
